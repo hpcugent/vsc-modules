3b8bade34338d232eb26f5960920b7a61d398570
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-04-22 11:33:46 +0200

    version bump, stich to mock < 4.0 when testing with Python 2

--- setup.py
+++ setup.py
@@ -3,25 +3,32 @@
 """
 Setup file for some modules related tooling
 """
+import sys
 
 import vsc.install.shared_setup as shared_setup
 from vsc.install.shared_setup import sdw
 
+if sys.version_info > (3, 0):
+    mock = 'mock'
+else:
+    # mock 4.x is no longer compatible with Python 2
+    mock = 'mock < 4.0'
+
 PACKAGE = {
-    'version': '0.0.4',
+    'version': '0.1.0',
     'author': [sdw],
     'maintainer': [sdw],
     'setup_requires': [
-        'vsc-install >= 0.11.5',
+        'vsc-install >= 0.15.3',
     ],
     'install_requires': [
-        'vsc-base',
+        'vsc-base >= 3.1.1',  # fix for stdin.write in Python 3
         'vsc-config',
         'vsc-utils',
         'atomicwrites',
     ],
     'tests_require': [
-        'mock',
+        mock,
     ],
 }
 
